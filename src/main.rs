2e427acebc25dc604634b9cbc2e30e50e7ca0767
René Rössler <rene@freshx.de>
2024-02-21 18:06:48 +0100

    web dark

--- src/main.rs
+++ src/main.rs
@@ -30,7 +30,10 @@ fn main() {
     // Redirect `log` message to `console.log` and friends:
     eframe::WebLogger::init(log::LevelFilter::Debug).ok();
 
-    let web_options = eframe::WebOptions::default();
+    let web_options = eframe::WebOptions {
+        follow_system_theme: false,
+        ..Default::default()
+    };
 
     wasm_bindgen_futures::spawn_local(async {
         eframe::WebRunner::new()
