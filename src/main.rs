395ee04638c32cc3ef5d1d2440f8e34b0b9d8292
René Rössler <rene@freshx.de>
2024-02-21 15:28:35 +0100

    fixes

--- src/main.rs
+++ src/main.rs
@@ -37,7 +37,7 @@ fn main() {
             .start(
                 "the_canvas_id", // hardcode it
                 web_options,
-                Box::new(|cc| Box::new(eframe_template::TemplateApp::new(cc))),
+                Box::new(|cc| Box::new(ui_experiments::TemplateApp::new(cc))),
             )
             .await
             .expect("failed to start eframe");
